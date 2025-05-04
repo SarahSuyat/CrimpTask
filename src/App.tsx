c40b9ef7e192130f3e37c1303844730b24df5fe4
SarahSuyat <suyatsarah519@gmail.com>
2025-05-04 21:42:23 +0800

    added Login and Register also reconfigure for routing

--- src/App.tsx
+++ src/App.tsx
@@ -32,6 +32,7 @@ import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import Login from './pages/Login';
 
 setupIonicReact();
 
@@ -39,11 +40,11 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
+      <Route exact path="/CrimpTask/home">
           <Home />
         </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
+        <Route exact path="/CrimpTask/">
+           <Redirect to="/CrimpTask/home" />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
---
+++ src/pages/Login.tsx
@@ -0,0 +1,64 @@
+
+import { 
+    IonButton,
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonMenuButton, 
+      IonPage, 
+      IonTitle, 
+      IonToolbar ,
+      IonInput, IonItem, IonList,
+      useIonRouter,
+      IonInputPasswordToggle,
+      IonIcon
+  } from '@ionic/react';
+  import { logoIonic } from 'ionicons/icons';
+  
+  const Login: React.FC = () => {
+    const navigation = useIonRouter();
+  
+    const doLogin = () => {
+        navigation.push('/it35-lab/app','forward','replace');
+      };
+  
+      const goToRegister = () => {
+        navigation.push('/register'); //  Navigate to Register
+      };
+  
+    
+    return (
+      <IonPage>
+        
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle >Login</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+  
+        <IonItem>
+        <IonInput label="Email" labelPlacement="floating" placeholder="Enter email"></IonInput>
+        </IonItem>
+  
+        <IonItem>
+          <IonInput label="Password" labelPlacement="floating" placeholder="Enter password">   
+          <IonInputPasswordToggle slot="end"></IonInputPasswordToggle> 
+          </IonInput>
+        </IonItem>
+  
+        <IonContent className= 'ion-padding'>
+            <IonButton onClick={() => doLogin()} expand="full">
+                Login
+            </IonButton>
+  
+            <IonButton onClick={() => goToRegister()} expand="full">
+                Sign up
+            </IonButton>
+            
+    
+        </IonContent>
+      </IonPage>
+    );
+  };
+  
+  export default Login;
