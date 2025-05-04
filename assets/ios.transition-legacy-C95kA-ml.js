80277d16bf345c7409bc00bd6fc1ab8841b5ee69
SarahSuyat <suyatsarah519@gmail.com>
2025-05-05 00:56:17 +0800

    Updates

--- assets/ios.transition-legacy-C95kA-ml.js
+++ assets/ios.transition-legacy-C95kA-ml.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-w0wwR6ss.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-DVmGpiql.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
