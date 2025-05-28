ed8711867189b223701e5d161a7a16dedb0f6b44
JanineSuello <janinegrasyanasuello@gmail.com>
2025-05-29 05:19:16 +0800

    Updates

--- assets/ios.transition-legacy-DqeoYd1-.js
+++ assets/ios.transition-legacy-DqeoYd1-.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CNrhCCZf.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-BerbKZyQ.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
