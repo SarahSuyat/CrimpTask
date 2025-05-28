c60ce9d8343c0d43ff376046b706133fae4ab962
JanineSuello <janinegrasyanasuello@gmail.com>
2025-05-29 05:01:37 +0800

    Updates

--- assets/swipe-back-legacy-hsyJLUJT.js
+++ assets/swipe-back-legacy-hsyJLUJT.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-B1BaiavQ.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CNrhCCZf.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
